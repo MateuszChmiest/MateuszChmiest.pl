55b41176ff8d25e4f52ac095775b595993793a2a
MateuszChmiest <mateusz_269@icloud.coml>
2022-05-13 02:11:40 +0200

    styling the social button

--- js/app.js
+++ js/app.js
@@ -14,7 +14,24 @@ const goTop = document.querySelector('.go_top');
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 200) {
         goTop.classList.add('visible');
+        social.classList.add('show');
     } else {
         goTop.classList.remove('visible');
+        social.classList.remove('show');
     }
+});
+
+// Social button
+
+const social = document.querySelector('.social');
+const socialSection = document.querySelector('.social-icon')
+
+social.addEventListener('click' ,() => {
+    socialSection.classList.add('show');
+    social.classList.add('hide');
+});
+
+socialSection.addEventListener('click' ,() => {
+    socialSection.classList.remove('show');
+    social.classList.remove('hide');
 });
